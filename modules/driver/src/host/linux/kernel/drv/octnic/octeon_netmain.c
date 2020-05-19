4ca2af7c4030f0786cf8d49a53fccf193408069c
Satha Rao <skoteshwar@marvell.com>
2020-05-19 12:50:22 -0400

    octnic: fix device features to enable ethtool settings
    Change-Id: Ifccf3f9fa4aaad1bbcf7c9b528cf8de7b1909e41
    Signed-off-by: Satha Rao <skoteshwar@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/firewall_offload/generic/host/octeontx/kernel/pcie_host/+/28711
    Reviewed-by: Satananda Burla <sburla@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>

--- modules/driver/src/host/linux/kernel/drv/octnic/octeon_netmain.c
+++ modules/driver/src/host/linux/kernel/drv/octnic/octeon_netmain.c
@@ -627,8 +627,8 @@ octnet_setup_nic_device(int octeon_id, oct_link_info_t * link_info, int ifidx)
 	oct_set_ethtool_ops(pndev);
 
 #if !defined(ETHERPCI)
-	pndev->features = NETIF_F_HW_CSUM;
-	pndev->features |= NETIF_F_SG;
+	pndev->hw_features = NETIF_F_HW_CSUM;
+	pndev->hw_features |= NETIF_F_SG;
 #if 0
 	/* Notify the n/w stack regarding TSO capability feature */
 	if ((oct->chip_id == OCTEON_CN83XX_PF)
@@ -704,6 +704,8 @@ octnet_setup_nic_device(int octeon_id, oct_link_info_t * link_info, int ifidx)
 	}
 
 	OCTNET_IFSTATE_SET(priv, OCT_NIC_IFSTATE_DROQ_OPS);
+
+	pndev->features |= pndev->hw_features;
 
 	/* Register the network device with the OS */
 	if (register_netdev(pndev)) {
