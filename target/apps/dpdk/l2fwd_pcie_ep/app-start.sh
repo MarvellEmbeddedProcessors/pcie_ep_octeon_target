0902671d342f5afce437a53d4a930cf8ed168d7f
Veerasenareddy Burru <vburru@cavium.com>
2020-09-28 21:16:48 -0700

    pcie_ep_octeontx: fix l2fwd app start script
    update to use correct path for config file and application binary
    
    Signed-off-by: Veerasenareddy Burru <vburru@cavium.com>
    Change-Id: I404f6b73d1cc14facf7e6bd4d3351ac1093d9521
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/pcie_ep/pcie_ep_octeontx/+/36911
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Satananda Burla <sburla@marvell.com>

--- target/apps/dpdk/l2fwd_pcie_ep/app-start.sh
+++ target/apps/dpdk/l2fwd_pcie_ep/app-start.sh
@@ -6,4 +6,4 @@ echo 24 > /proc/sys/vm/nr_hugepages
 
 # 0002:0c:00.0 is NIX PF and 0002:0f:00.1 is SDP VF
 dpdk-devbind -b vfio-pci 0002:0c:00.0 0002:0f:00.1
-/usr/bin/l2fwd_otx2  -l 0-3 -n 4 -w 0002:0c:00.0 -w 0002:0f:00.1 -- -q 8 -p 0x3 -f /root/conf.csv --no-mac-updating
+/usr/bin/l2fwd_pcie_ep  -l 0-3 -n 4 -w 0002:0c:00.0 -w 0002:0f:00.1 -- -q 8 -p 0x3 -f /etc/l2fwd_conf.csv --no-mac-updating
