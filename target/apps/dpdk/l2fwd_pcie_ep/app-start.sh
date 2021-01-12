ecfc861c0a23e6563b8fe15577b1dfc86f3900f8
Burla, Satananda <sburla@marvell.com>
2021-01-13 00:47:58 +0530

    l2fwd_pcie_ep: Updated startup script
    Updated startup script to fix CGX PF.
    Updated VF for VF test
    
    Signed-off-by: Burla, Satananda <sburla@marvell.com>
    Change-Id: I040bf56c98cc9b725919eb722cf03f20aaf1682b
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/pcie_ep/pcie_ep_octeontx/+/43512
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>

--- target/apps/dpdk/l2fwd_pcie_ep/app-start.sh
+++ target/apps/dpdk/l2fwd_pcie_ep/app-start.sh
@@ -3,10 +3,26 @@
 
 #!/bin/sh
 
+CGX_PF="0002:05:00.0"
+SDP_VF_FOR_HOST_PF="0002:0f:00.1"
+SDP_VF_FOR_HOST_VF="0002:0f:00.2"
+
+SDP_VF=$SDP_VF_FOR_HOST_PF
+if [ "$1" = "vf" ]; then
+	echo "Host VF test"
+        SDP_VF=$SDP_VF_FOR_HOST_VF
+fi
+
 mkdir -p /dev/huge
 mount -t hugetlbfs none /dev/huge
 echo 24 > /proc/sys/vm/nr_hugepages
 
-# 0002:0c:00.0 is NIX PF and 0002:0f:00.1 is SDP VF
-dpdk-devbind -b vfio-pci 0002:0c:00.0 0002:0f:00.1
-/usr/bin/l2fwd_pcie_ep  -l 0-3 -n 4 -w 0002:0c:00.0 -w 0002:0f:00.1 -- -q 8 -p 0x3 -f /etc/l2fwd_conf.csv --no-mac-updating
+while [ ! -f /sys/bus/pci/devices/0002\:0f\:00.0/sdp_ring_attr/sdp_vf0_rings ]
+do
+     sleep 10;
+done
+echo "Handshake with host complete; continuing with loading application..."
+
+dpdk-devbind -b vfio-pci $CGX_PF $SDP_VF
+
+/usr/bin/l2fwd_pcie_ep  -l 0-3 -n 4 -w $CGX_PF -w $SDP_VF -- -q 8 -p 0x3 -f /etc/l2fwd_conf.csv --no-mac-updating
