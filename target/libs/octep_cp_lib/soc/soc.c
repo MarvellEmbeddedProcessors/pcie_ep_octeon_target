db844dc90f1b8ea5b0a504512431de89e378c70a
Abhijit Ayarekar <aayarekar@marvell.com>
2022-07-07 09:04:25 +0000

    octep_cp_lib: SoC autodetection.
    Autodetection code is taken from dpdk roc_model.
    Most of the code is retained as is so that synching of future Soc is easier.
    
    Change-Id: Iddcada70e9aabd33f6450d63363c36815d899835
    Signed-off-by: Abhijit Ayarekar <aayarekar@marvell.com>

--- target/libs/octep_cp_lib/soc/soc.c
+++ target/libs/octep_cp_lib/soc/soc.c
@@ -5,6 +5,12 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #include "octep_cp_lib.h"
 #include "cp_log.h"
@@ -13,6 +19,390 @@
 #include "cnxk.h"
 #include "cnxk_loop.h"
 #include "cnxk_nic.h"
+
+#ifndef BIT_ULL
+#define BIT_ULL(nr) (1ULL << (nr))
+#endif
+
+struct soc_model {
+#define SOC_MODEL_CN96xx_A0    BIT_ULL(0)
+#define SOC_MODEL_CN96xx_B0    BIT_ULL(1)
+#define SOC_MODEL_CN96xx_C0    BIT_ULL(2)
+#define SOC_MODEL_CNF95xx_A0   BIT_ULL(4)
+#define SOC_MODEL_CNF95xx_B0   BIT_ULL(6)
+#define SOC_MODEL_CNF95xxMM_A0 BIT_ULL(8)
+#define SOC_MODEL_CNF95xxN_A0  BIT_ULL(12)
+#define SOC_MODEL_CNF95xxO_A0  BIT_ULL(13)
+#define SOC_MODEL_CNF95xxN_A1  BIT_ULL(14)
+#define SOC_MODEL_CNF95xxN_B0  BIT_ULL(15)
+#define SOC_MODEL_CN98xx_A0    BIT_ULL(16)
+#define SOC_MODEL_CN98xx_A1    BIT_ULL(17)
+#define SOC_MODEL_CN106xx_A0   BIT_ULL(20)
+#define SOC_MODEL_CNF105xx_A0  BIT_ULL(21)
+#define SOC_MODEL_CNF105xxN_A0 BIT_ULL(22)
+#define SOC_MODEL_CN103xx_A0   BIT_ULL(23)
+#define SOC_MODEL_CN106xx_A1   BIT_ULL(24)
+/* Following flags describe platform code is running on */
+#define SOC_ENV_HW   BIT_ULL(61)
+#define SOC_ENV_EMUL BIT_ULL(62)
+#define SOC_ENV_ASIM BIT_ULL(63)
+
+	uint64_t flag;
+#define SOC_MODEL_STR_LEN_MAX 128
+	char name[SOC_MODEL_STR_LEN_MAX];
+	char env[SOC_MODEL_STR_LEN_MAX];
+};
+
+#define SOC_MODEL_CN96xx_Ax (SOC_MODEL_CN96xx_A0 | SOC_MODEL_CN96xx_B0)
+#define SOC_MODEL_CN98xx_Ax (SOC_MODEL_CN98xx_A0 | SOC_MODEL_CN98xx_A1)
+#define SOC_MODEL_CN9K                                                         \
+	(SOC_MODEL_CN96xx_Ax | SOC_MODEL_CN96xx_C0 | SOC_MODEL_CNF95xx_A0 |    \
+	 SOC_MODEL_CNF95xx_B0 | SOC_MODEL_CNF95xxMM_A0 |                       \
+	 SOC_MODEL_CNF95xxO_A0 | SOC_MODEL_CNF95xxN_A0 | SOC_MODEL_CN98xx_Ax | \
+	 SOC_MODEL_CNF95xxN_A1 | SOC_MODEL_CNF95xxN_B0)
+#define SOC_MODEL_CNF9K                                                        \
+	(SOC_MODEL_CNF95xx_A0 | SOC_MODEL_CNF95xx_B0 |                         \
+	 SOC_MODEL_CNF95xxMM_A0 | SOC_MODEL_CNF95xxO_A0 |                      \
+	 SOC_MODEL_CNF95xxN_A0 | SOC_MODEL_CNF95xxN_A1 |                       \
+	 SOC_MODEL_CNF95xxN_B0)
+
+#define SOC_MODEL_CN106xx   (SOC_MODEL_CN106xx_A0 | SOC_MODEL_CN106xx_A1)
+#define SOC_MODEL_CNF105xx  (SOC_MODEL_CNF105xx_A0)
+#define SOC_MODEL_CNF105xxN (SOC_MODEL_CNF105xxN_A0)
+#define SOC_MODEL_CN103xx   (SOC_MODEL_CN103xx_A0)
+#define SOC_MODEL_CN10K                                                        \
+	(SOC_MODEL_CN106xx | SOC_MODEL_CNF105xx | SOC_MODEL_CNF105xxN |        \
+	 SOC_MODEL_CN103xx)
+#define SOC_MODEL_CNF10K (SOC_MODEL_CNF105xx | SOC_MODEL_CNF105xxN)
+
+/* RoC and CPU IDs and revisions */
+#define VENDOR_ARM    0x41 /* 'A' */
+#define VENDOR_CAVIUM 0x43 /* 'C' */
+
+#define SOC_PART_CN10K 0xD49
+
+#define PART_106xx  0xB9
+#define PART_105xx  0xBA
+#define PART_105xxN 0xBC
+#define PART_103xx  0xBD
+#define PART_98xx   0xB1
+#define PART_96xx   0xB2
+#define PART_95xx   0xB3
+#define PART_95xxN  0xB4
+#define PART_95xxMM 0xB5
+#define PART_95O    0xB6
+
+#define MODEL_IMPL_BITS	  8
+#define MODEL_IMPL_SHIFT  24
+#define MODEL_IMPL_MASK	  ((1 << MODEL_IMPL_BITS) - 1)
+#define MODEL_PART_BITS	  12
+#define MODEL_PART_SHIFT  4
+#define MODEL_PART_MASK	  ((1 << MODEL_PART_BITS) - 1)
+#define MODEL_MAJOR_BITS  4
+#define MODEL_MAJOR_SHIFT 20
+#define MODEL_MAJOR_MASK  ((1 << MODEL_MAJOR_BITS) - 1)
+#define MODEL_MINOR_BITS  4
+#define MODEL_MINOR_SHIFT 0
+#define MODEL_MINOR_MASK  ((1 << MODEL_MINOR_BITS) - 1)
+
+#define MODEL_CN10K_PART_SHIFT	8
+#define MODEL_CN10K_PASS_BITS	4
+#define MODEL_CN10K_PASS_MASK	((1 << MODEL_CN10K_PASS_BITS) - 1)
+#define MODEL_CN10K_MAJOR_BITS	2
+#define MODEL_CN10K_MAJOR_SHIFT 2
+#define MODEL_CN10K_MAJOR_MASK	((1 << MODEL_CN10K_MAJOR_BITS) - 1)
+#define MODEL_CN10K_MINOR_BITS	2
+#define MODEL_CN10K_MINOR_SHIFT 0
+#define MODEL_CN10K_MINOR_MASK	((1 << MODEL_CN10K_MINOR_BITS) - 1)
+
+#define	ARR_DIM(a)	(sizeof (a) / sizeof ((a)[0]))
+
+static const struct model_db {
+	uint32_t impl;
+	uint32_t part;
+	uint32_t major;
+	uint32_t minor;
+	uint64_t flag;
+	char name[SOC_MODEL_STR_LEN_MAX];
+} model_db[] = {
+	{VENDOR_ARM, PART_106xx, 0, 0, SOC_MODEL_CN106xx_A0, "cn10ka_a0"},
+	{VENDOR_ARM, PART_106xx, 0, 1, SOC_MODEL_CN106xx_A1, "cn10ka_a1"},
+	{VENDOR_ARM, PART_105xx, 0, 0, SOC_MODEL_CNF105xx_A0, "cnf10ka_a0"},
+	{VENDOR_ARM, PART_103xx, 0, 0, SOC_MODEL_CN103xx_A0, "cn10kb_a0"},
+	{VENDOR_ARM, PART_105xxN, 0, 0, SOC_MODEL_CNF105xxN_A0, "cnf10kb_a0"},
+	{VENDOR_CAVIUM, PART_98xx, 0, 0, SOC_MODEL_CN98xx_A0, "cn98xx_a0"},
+	{VENDOR_CAVIUM, PART_98xx, 0, 1, SOC_MODEL_CN98xx_A1, "cn98xx_a1"},
+	{VENDOR_CAVIUM, PART_96xx, 0, 0, SOC_MODEL_CN96xx_A0, "cn96xx_a0"},
+	{VENDOR_CAVIUM, PART_96xx, 0, 1, SOC_MODEL_CN96xx_B0, "cn96xx_b0"},
+	{VENDOR_CAVIUM, PART_96xx, 2, 0, SOC_MODEL_CN96xx_C0, "cn96xx_c0"},
+	{VENDOR_CAVIUM, PART_96xx, 2, 1, SOC_MODEL_CN96xx_C0, "cn96xx_c1"},
+	{VENDOR_CAVIUM, PART_95xx, 0, 0, SOC_MODEL_CNF95xx_A0, "cnf95xx_a0"},
+	{VENDOR_CAVIUM, PART_95xx, 1, 0, SOC_MODEL_CNF95xx_B0, "cnf95xx_b0"},
+	{VENDOR_CAVIUM, PART_95xxN, 0, 0, SOC_MODEL_CNF95xxN_A0, "cnf95xxn_a0"},
+	{VENDOR_CAVIUM, PART_95xxN, 0, 1, SOC_MODEL_CNF95xxN_A0, "cnf95xxn_a1"},
+	{VENDOR_CAVIUM, PART_95xxN, 1, 0, SOC_MODEL_CNF95xxN_B0, "cnf95xxn_b0"},
+	{VENDOR_CAVIUM, PART_95O, 0, 0, SOC_MODEL_CNF95xxO_A0, "cnf95O_a0"},
+	{VENDOR_CAVIUM, PART_95xxMM, 0, 0, SOC_MODEL_CNF95xxMM_A0,
+	 "cnf95xxmm_a0"}};
+
+static struct soc_model model;
+
+#define PCI_VENDOR_ID_CAVIUM		0x177D
+#define PCI_DEVID_CNXK_RVU_PF		0xA063
+#define PCI_DEVID_CNXK_RVU_VF		0xA064
+#define PCI_DEVID_CNXK_RVU_AF		0xA065
+#define PCI_DEVID_CN10K_RVU_CPT_PF	0xA0F2
+#define PCI_DEVID_CN10K_RVU_CPT_VF	0xA0F3
+#define PCI_DEVID_CNXK_RVU_AF_VF	0xA0f8
+#define PCI_DEVID_CNXK_RVU_SSO_TIM_PF	0xA0F9
+#define PCI_DEVID_CNXK_RVU_SSO_TIM_VF	0xA0FA
+#define PCI_DEVID_CNXK_RVU_NPA_PF	0xA0FB
+#define PCI_DEVID_CNXK_RVU_NPA_VF	0xA0FC
+
+/* Detect if RVU device */
+static bool is_rvu_device(unsigned long val)
+{
+	return (val == PCI_DEVID_CNXK_RVU_PF || val == PCI_DEVID_CNXK_RVU_VF ||
+		val == PCI_DEVID_CNXK_RVU_AF ||
+		val == PCI_DEVID_CNXK_RVU_AF_VF ||
+		val == PCI_DEVID_CNXK_RVU_NPA_PF ||
+		val == PCI_DEVID_CNXK_RVU_NPA_VF ||
+		val == PCI_DEVID_CNXK_RVU_SSO_TIM_PF ||
+		val == PCI_DEVID_CNXK_RVU_SSO_TIM_VF ||
+		val == PCI_DEVID_CN10K_RVU_CPT_PF ||
+		val == PCI_DEVID_CN10K_RVU_CPT_VF);
+}
+
+/* parse a sysfs (or other) file containing one integer value */
+static int parse_sysfs_value(const char *filename, unsigned long *val)
+{
+	FILE *f;
+	char buf[BUFSIZ];
+	char *end = NULL;
+
+	if ((f = fopen(filename, "r")) == NULL) {
+		CP_LIB_LOG(ERR, SOC, "Cannot open sysfs value %s\n", filename);
+		return -EIO;
+	}
+
+	if (fgets(buf, sizeof(buf), f) == NULL) {
+		CP_LIB_LOG(ERR, SOC, "Cannot read sysfs value %s\n", filename);
+		fclose(f);
+		return -EIO;
+	}
+	*val = strtoul(buf, &end, 0);
+	if ((buf[0] == '\0') || (end == NULL) || (*end != '\n')) {
+		CP_LIB_LOG(ERR, SOC, "Cannot parse sysfs value %s\n", filename);
+		fclose(f);
+		return -EIO;
+	}
+	fclose(f);
+	return 0;
+}
+
+static int rvu_device_lookup(const char *dirname, uint32_t *part,
+			     uint32_t *pass)
+{
+	char filename[PATH_MAX];
+	unsigned long val;
+
+	/* Check if vendor id is cavium */
+	snprintf(filename, sizeof(filename), "%s/vendor", dirname);
+	if (parse_sysfs_value(filename, &val) < 0)
+		goto error;
+
+	if (val != PCI_VENDOR_ID_CAVIUM)
+		goto error;
+
+	/* Get device id  */
+	snprintf(filename, sizeof(filename), "%s/device", dirname);
+	if (parse_sysfs_value(filename, &val) < 0)
+		goto error;
+
+	/* Check if device ID belongs to any RVU device */
+	if (!is_rvu_device(val))
+		goto error;
+
+	/* Get subsystem_device id */
+	snprintf(filename, sizeof(filename), "%s/subsystem_device", dirname);
+	if (parse_sysfs_value(filename, &val) < 0)
+		goto error;
+
+	*part = val >> MODEL_CN10K_PART_SHIFT;
+
+	/* Get revision for pass value*/
+	snprintf(filename, sizeof(filename), "%s/revision", dirname);
+	if (parse_sysfs_value(filename, &val) < 0)
+		goto error;
+
+	*pass = val & MODEL_CN10K_PASS_MASK;
+
+	return 0;
+error:
+	return -EINVAL;
+}
+
+/* Scans through all PCI devices, detects RVU device and returns
+ * subsystem_device
+ */
+static int cn10k_part_pass_get(uint32_t *part, uint32_t *pass)
+{
+#define SYSFS_PCI_DEVICES "/sys/bus/pci/devices"
+	char dirname[4064];
+	struct dirent *e;
+	DIR *dir;
+
+	dir = opendir(SYSFS_PCI_DEVICES);
+	if (dir == NULL) {
+		CP_LIB_LOG(ERR, SOC, "opendir failed: %s\n", strerror(errno));
+		return -errno;
+	}
+
+	while ((e = readdir(dir)) != NULL) {
+		if (e->d_name[0] == '.')
+			continue;
+
+		snprintf(dirname, sizeof(dirname), "%s/%s", SYSFS_PCI_DEVICES,
+			 e->d_name);
+
+		/* Lookup for rvu device and get part pass information */
+		if (!rvu_device_lookup(dirname, part, pass))
+			break;
+	}
+
+	closedir(dir);
+	return 0;
+}
+
+static int populate_model(uint32_t midr)
+{
+	uint32_t impl, major, part, minor, pass;
+	int ret = -ENODEV;
+	size_t i;
+
+	impl = (midr >> MODEL_IMPL_SHIFT) & MODEL_IMPL_MASK;
+	part = (midr >> MODEL_PART_SHIFT) & MODEL_PART_MASK;
+	major = (midr >> MODEL_MAJOR_SHIFT) & MODEL_MAJOR_MASK;
+	minor = (midr >> MODEL_MINOR_SHIFT) & MODEL_MINOR_MASK;
+
+	/* Update part number for cn10k from device-tree */
+	if (part == SOC_PART_CN10K) {
+		if (cn10k_part_pass_get(&part, &pass))
+			goto not_found;
+		/*
+		 * Pass value format:
+		 * Bits 0..1: minor pass
+		 * Bits 3..2: major pass
+		 */
+		minor = (pass >> MODEL_CN10K_MINOR_SHIFT) &
+			MODEL_CN10K_MINOR_MASK;
+		major = (pass >> MODEL_CN10K_MAJOR_SHIFT) &
+			MODEL_CN10K_MAJOR_MASK;
+	}
+
+	for (i = 0; i < ARR_DIM(model_db); i++)
+		if (model_db[i].impl == impl && model_db[i].part == part &&
+		    model_db[i].major == major && model_db[i].minor == minor) {
+			model.flag = model_db[i].flag;
+			strncpy(model.name, model_db[i].name,
+				SOC_MODEL_STR_LEN_MAX - 1);
+			ret = 0;
+			break;
+		}
+not_found:
+	if (ret) {
+		model.flag = 0;
+		strncpy(model.name, "unknown", SOC_MODEL_STR_LEN_MAX - 1);
+		CP_LIB_LOG(ERR, SOC,
+			   "Invalid SoC model "
+			   "(impl=0x%x, part=0x%x, major=0x%x, minor=0x%x)",
+			   impl, part, major, minor);
+	}
+
+	return ret;
+}
+
+static int midr_get(unsigned long *val)
+{
+	const char *file =
+		"/sys/devices/system/cpu/cpu0/regs/identification/midr_el1";
+	int rc = -EIO;
+	char buf[BUFSIZ];
+	char *end = NULL;
+	FILE *f;
+
+	if (val == NULL)
+		goto err;
+	f = fopen(file, "r");
+	if (f == NULL)
+		goto err;
+
+	if (fgets(buf, sizeof(buf), f) == NULL)
+		goto fclose;
+
+	*val = strtoul(buf, &end, 0);
+	if ((buf[0] == '\0') || (end == NULL) || (*end != '\n'))
+		goto fclose;
+
+	rc = 0;
+fclose:
+	fclose(f);
+err:
+	return rc;
+}
+
+static void of_env_get()
+{
+	const char *const path = "/proc/device-tree/soc@0/runplatform";
+	const char *const hw_plt = "HW_PLATFORM";
+	FILE *fp;
+
+	if (access(path, F_OK) != 0) {
+		strncpy(model.env, "HW_PLATFORM", SOC_MODEL_STR_LEN_MAX - 1);
+		model.flag = SOC_ENV_HW;
+		return;
+	}
+
+	fp = fopen(path, "r");
+	if (!fp) {
+		CP_LIB_LOG(ERR, SOC, "Failed to open %s", path);
+		return;
+	}
+
+	if (!fgets(model.env, sizeof(model.env), fp)) {
+		CP_LIB_LOG(ERR, SOC, "Failed to read %s", path);
+		goto err;
+	}
+
+	/* we only support hardware platform */
+	if (strncmp(hw_plt, model.env, strlen(hw_plt))) {
+		CP_LIB_LOG(ERR, SOC, "Unknown platform: %s", model.env);
+		goto err;
+	}
+
+	model.flag = SOC_ENV_HW;
+err:
+	fclose(fp);
+}
+
+static int detect_soc()
+{
+	unsigned long midr;
+	int err;
+
+	err = midr_get(&midr);
+	if (err)
+		return err;
+
+	err = populate_model(midr);
+	if (err)
+		return err;
+
+	of_env_get();
+	CP_LIB_LOG(INFO, SOC, "Model: %s (%s)\n", model.name, model.env);
+
+	return 0;
+}
 
 static struct cp_lib_soc_ops ops[CP_LIB_SOC_MAX][OCTEP_CP_MODE_MAX] = {
 	/* cnxk */
@@ -47,17 +437,19 @@ static struct cp_lib_soc_ops ops[CP_LIB_SOC_MAX][OCTEP_CP_MODE_MAX] = {
 	}
 };
 
-static enum cp_lib_soc detect_soc()
-{
-	return CP_LIB_SOC_OTX2;
-}
-
 int soc_get_ops(enum octep_cp_mode mode, struct cp_lib_soc_ops **p_ops)
 {
-	cfg.soc = detect_soc();
-	if (cfg.soc >= CP_LIB_SOC_MAX || mode >= OCTEP_CP_MODE_MAX || !p_ops) {
-		CP_LIB_LOG(INFO, SOC, "Invalid param "
-			   "soc:%d mode:%d p_ops:%p\n", cfg.soc, mode, p_ops);
+	int err;
+
+	err = detect_soc();
+	if (err)
+		return err;
+
+	cfg.soc = (model.flag & (SOC_MODEL_CN10K)) ?
+		   CP_LIB_SOC_CNXK : CP_LIB_SOC_OTX2;
+	if (mode >= OCTEP_CP_MODE_MAX || !p_ops) {
+		CP_LIB_LOG(INFO, SOC, "Invalid param: mode:%d p_ops:%p\n",
+			   mode, p_ops);
 		return -EINVAL;
 	}
 	*p_ops = &ops[cfg.soc][mode];
