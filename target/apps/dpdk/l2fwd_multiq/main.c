5215ad15bf330c1c3910d86227c0113b9df9b5e7
Radha Mohan Chintakuntla <radhac@marvell.com>
2022-08-02 13:03:01 -0700

    l2fwd_multiq: remove undefined variable for dpdk-21.11
    The max_rx_pkt_len is removed in dpdk-21.11.
    
    Signed-off-by: Radha Mohan Chintakuntla <radhac@marvell.com>
    Change-Id: I4139d43cb16a92b568f150621fb7fd9da82c036d

--- target/apps/dpdk/l2fwd_multiq/main.c
+++ target/apps/dpdk/l2fwd_multiq/main.c
@@ -576,7 +576,6 @@ main(int argc, char **argv)
 	struct rte_eth_conf port_conf = {
 		.rxmode = {
 			.mq_mode = ETH_MQ_RX_RSS,
-			.max_rx_pkt_len = RTE_ETHER_MAX_LEN,
 			.split_hdr_size = 0,
 		},
 		.rx_adv_conf = {
