f562f5103b0f7f3f134b63794260bb2e155e144c
Shinas Rasheed <srasheed@marvell.com>
2025-04-08 03:29:33 -0700

    octep_cp_lib: remove DPI arg and map BAR4 from SDP RVU pf
    Remove DPI PF argument as DPI PF shall be initialised by DPI kernel
    driver. Map EPF BAR4 for mbox from first SDP RVU pf. Hence both
    SDP RVU pfs have to be provided as comma separated list of arguments
    
    Usage changes to:
    
    ./octep_cp_agent /usr/bin/cn106xx.cfg -- --sdp_rvu_pf
    0002:18:00.0,0002:19:00.0 --pem_dev 0001:00:10.0
    
    Signed-off-by: Shinas Rasheed <srasheed@marvell.com>
    Change-Id: I928245479194b3ffbfaca57aebccc6160ca235d8

--- target/libs/octep_cp_lib/main.c
+++ target/libs/octep_cp_lib/main.c
@@ -25,7 +25,6 @@ struct octep_cp_lib_cfg *lib_cfg;
 static const char short_opts[] = {};
 static const struct option long_opts[] = {
 #if USE_PEM_AND_DPI_PF
-	{"dpi_dev", 1, 0, 'd'},
 	{"pem_dev", 1, 0, 'p'},
 	{"sdp_rvu_pf", 1, 0, 's'},
 #endif
@@ -43,10 +42,6 @@ int octep_cp_lib_parse_args(int argc, char **argv, struct octep_cp_lib_cfg *cfg)
 				  long_opts, &option_index)) != EOF) {
 		switch (opt) {
 #if USE_PEM_AND_DPI_PF
-		case 'd': /* DPI device */
-			if (cnxk_vfio_parse_dpi_dev(optarg))
-				ret = -1;
-			break;
 		case 'p': /* PEM device */
 			if (cnxk_vfio_parse_pem_dev(optarg))
 				ret = -1;
